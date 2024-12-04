7a8caa410c94a5378169c4cb36b126141bc7d095
greyfox80 <greyfox80@gmail.com>
2024-12-04 08:34:22 +0700

    Add files via upload

--- MyPythonScripts.py
+++ MyPythonScripts.py
@@ -8,25 +8,6 @@ from PIL import Image
 import time
 from annotated_text import annotated_text
 
-# Function to check Keras version compatibility (optional)
-def check_keras_version(saved_model_path):
-    # Implement logic to check the saved model's Keras version (e.g., from metadata)
-    # If versions differ, raise a warning or suggest installing a compatible version
-# Load Model Classification
-    try:
-        model = load_model('Model_Opsi_Data_Sampah_003.keras')
-        # Optional: Check Keras version compatibility using check_keras_version()
-    except OSError as e:
-        st.error(f"Error loading model: {e}")
-        exit(1)  # Exit gracefully
-    # Load Class Names (assuming pickle format)
-    try:
-        with open('class_names.pkl', 'rb') as f:  # Replace with your class name file path
-            class_names = pickle.load(f)
-    except FileNotFoundError:
-        st.error("Class names file not found.")
-    exit(1)
-
 # Title
 st.title("Welcome to 'EcoEye' Predictions")
 
@@ -49,8 +30,7 @@ with col3:
               )
 
 # Load Model Classification
-#model = pickle.load(open('D:/Model_Opsi_Data_Sampah_002.sav', 'rb'))
-model  = load_model('Model_Opsi_Data_Sampah_003.keras')
+#model  = load_model('Model_Opsi_Data_Sampah_003.keras')
 
 
 # Load_ Class Name
@@ -72,15 +52,15 @@ if file is not None:
         progress_bar.progress(perc_completed+1)
 
     # Classify image
-    class_name, index, prob = classify(image, model, class_names)
+    #class_name, index, prob = classify(image, model, class_names)
 
     st.markdown(" ###### Process Completed !")
 
-    with st.expander('Click for prediction Result :'):
-         #metrics = st.metric(label="Prediction", value=format(class_name2[index]), delta=format(prob))
-         st.write("## Prediction Level : {} ".format(class_name2[index]))
-         st.write("## Prediction Prob : {:.0%} ".format(prob, '.0%'))
-         st.write("## Action Recommendation : {} ".format(class_name3[index]))
+    #with st.expander('Click for prediction Result :'):
+    #     #metrics = st.metric(label="Prediction", value=format(class_name2[index]), delta=format(prob))
+    #     st.write("## Prediction Level : {} ".format(class_name2[index]))
+    #     st.write("## Prediction Prob : {:.0%} ".format(prob, '.0%'))
+    #     st.write("## Action Recommendation : {} ".format(class_name3[index]))
 
 
 
